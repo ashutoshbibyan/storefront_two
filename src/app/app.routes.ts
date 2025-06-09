beedefa14fc088572c9c39242e6e041aa215fe90
ashutosh <ashutosh2013official@gmail.com>
2025-06-09 13:10:58 +0530

    Day 1: Initial setup with hybrid structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {LayoutComponent} from './dashboard/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'',component:LayoutComponent,children:[
+      {path:'products',loadChildren:()=>import('./products/products.module').then(m => m.ProductsModule)},
+      {path:'customers',loadChildren:()=>import('./customers/customers.module').then(m => m.CustomersModule)},
+      {path:'bills', loadChildren:()=>import('./bills/bills.module').then(m => m.BillsModule)},
+      {path:'reports',loadChildren:()=>import('./reports/reports.module').then(m => m.ReportsModule)},
+    ]},
+
+];
