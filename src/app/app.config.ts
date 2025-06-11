a9e26528e2340e0079b9713e526e80d7a8ae423c
ashutosh <ashutosh2013official@gmail.com>
2025-06-11 14:14:24 +0530

    day 5 interceptor is added so it will send the auth token to backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {TokenInterceptor} from './auth/token.interceptor';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes) , provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes) , provideHttpClient(withInterceptors([TokenInterceptor]))]
 };
--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private httpClient:HttpClient) {
+    this.httpClient.get('http://localhost:3000/api/protected').subscribe({next(data){console.log(data)}});
+  }
 }
